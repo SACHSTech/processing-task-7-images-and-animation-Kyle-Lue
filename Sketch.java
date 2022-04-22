989314c60bf85efa4a5ae494422b25f2487b5536
Kyle-Lue <jeffrey.lue23@ycdsbk12.ca>
2022-04-21 23:45:51 -0400

    .

--- Sketch.java
+++ Sketch.java
@@ -3,8 +3,9 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-  PImage imgBackground = loadImage("Background.png");
-  imgBackground = imgBackground.resize(imgBackground.width * (float) 1.5625) , imgBackground.height * (float) 1.5625;
+  PImage imgBackground;
+  PImage imgTurtle;
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -12,6 +13,10 @@ public class Sketch extends PApplet {
     float fltCircleX = 50;
     float fltSpeedY = 2;
     float fltSpeedX = 3;
+    float fltImageX = 50;
+    float fltImageY = 50;
+    float fltImgSpeedX = 4;
+    float fltImgSpeedY = 2;
 
   public void settings() {
 	// put your size call here
@@ -23,7 +28,11 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    
+    imgBackground = loadImage("Background.png");
+    imgTurtle = loadImage("turtle.png");
+    int intHeight = (int)(imgBackground.height * (float)1.5625);
+    int intWidth = (int)(imgBackground.width *(float)1.5625);
+    imgBackground.resize(intWidth, intHeight);
   }
 
   /**
@@ -31,12 +40,19 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  background(32,32,32);
+    image(imgBackground, 0, 0);
     fill(125,67, 180);
+    image(imgTurtle, fltImageX, fltImageY);
     ellipse(fltCircleX, fltCircleY, 20, 20);
 
     fltCircleX = fltCircleX + fltSpeedX;
     fltCircleY = fltCircleY + fltSpeedY;
+    
+    fltImageX = fltImageX + fltImgSpeedX;
+    fltImageY = fltImageY + fltImgSpeedY;
+
     fltSpeedY = fltSpeedY + (float)0.1;
+    fltImgSpeedY = fltImgSpeedY + (float)0.001;
 
     //bounce off left and right
     if(fltCircleX < 0 || fltCircleX > width) {
@@ -46,6 +62,16 @@ public class Sketch extends PApplet {
    // bounce off top and bottom
     if(fltCircleY < 0 || fltCircleY > height) {
     fltSpeedY = fltSpeedY * -1;
+
+    //bounce off left and right
+    if(fltImageX < 0 +150 || fltImageX >= width - 150) {
+    fltImgSpeedX = fltImgSpeedX * -1;
+    }
+  
+     // bounce off top and bottom
+    if(fltImageY < 0 + 150 || fltImageY >= height - 150) {
+    fltImgSpeedY = fltImgSpeedY * -1;
+    }
   }
   
   // define other methods down here.
